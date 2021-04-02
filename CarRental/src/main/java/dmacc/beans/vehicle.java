7cee47ac7f250a38388aed0695d73f94820f62ee
Nathaniel Essick <nlessick@dmacc.edu>
2021-04-02 13:58:02 -0500

    added vehicle class

---
+++ CarRental/src/main/java/dmacc/beans/vehicle.java
@@ -0,0 +1,15 @@
+package dmacc.beans;
+
+/**
+ * @author {Nathaniel Essick} - nlessick
+ * CIS175 - Spring 2021
+ * {4/2/2021}
+ */
+public class vehicle {
+	private long id;
+	private String model;
+	private String make;
+	private int age;
+	private boolean availability;
+
+}
