58377d1c6693e0a7c288c953cf707eeaea6ace65
GabrielWoodburn <gwoodburn@dmacc.edu>
2021-04-06 18:46:03 -0500

    Initial Index Commit
    This is the initial commit for the index page in the static folder

--- CarRental/src/main/java/dmacc/beans/Dealership.java
+++ CarRental/src/main/java/dmacc/beans/Dealership.java
@@ -1,12 +1,6 @@
 package dmacc.beans;
 
-/**
- * @author {Nathaniel Essick} - nlessick
- * CIS175 - Spring 2021
- * {4/2/2021}
- */
 public class Dealership {
 	private long id;
 	private String name;
-
 }
--- CarRental/src/main/java/dmacc/beans/vehicle.java
+++ CarRental/src/main/java/dmacc/beans/vehicle.java
@@ -1,10 +1,5 @@
 package dmacc.beans;
 
-/**
- * @author {Nathaniel Essick} - nlessick
- * CIS175 - Spring 2021
- * {4/2/2021}
- */
 public class vehicle {
 	private long id;
 	private String model;
