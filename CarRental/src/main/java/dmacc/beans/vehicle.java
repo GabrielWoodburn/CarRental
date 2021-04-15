4ec0ce140cb38f58e84d4e69fe3ddd42b0099c3d
GabrielWoodburn <gwoodburn@dmacc.edu>
2021-04-15 17:03:19 -0500

    Commit contains WebController, DealershipRepository, and minor annotations with changes to index.html
    This commit contains the WebController to begin to route the pages and the DealershipRepository interface. It also contains an addition of the Getter and Setter annotations to the entities and an update to the links on the index page to direct to the dealerhsips.

--- CarRental/src/main/java/dmacc/beans/vehicle.java
+++ CarRental/src/main/java/dmacc/beans/vehicle.java
@@ -1,5 +1,13 @@
 package dmacc.beans;
 
+import javax.persistence.Embeddable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Embeddable
 public class vehicle {
 	private long id;
 	private String model;
