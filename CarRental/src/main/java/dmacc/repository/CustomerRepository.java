dda6be121d5fb2f1957bf0859f5493baefa5843e
rchinhamhora <rchinhamhora@gmail.com>
2021-04-25 00:07:45 -0500

    Repos with Two New and One Updated
    CustomerRepository.java  -updated
    RentalRepository.java -   New
    VehicleRepository.java -New

--- CarRental/src/main/java/dmacc/repository/CustomerRepository.java
+++ CarRental/src/main/java/dmacc/repository/CustomerRepository.java
@@ -1,16 +1,15 @@
 package dmacc.repository;
 
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-
 import dmacc.beans.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 /**
  * Rumbi Chinhamhora rchinhamhora
  * CIS 175 - Spring 2021
  * Apr 10, 2021
  */
-public interface CustomerRepository extends CrudRepository<Customer, Long>{
+public interface CustomerRepository extends JpaRepository<Customer, Long>{
 	List<Customer> findAll();
 }
